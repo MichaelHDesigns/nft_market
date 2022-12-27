de1e9c761812e388a33aa18dde1cbe345423dfad
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-27 01:02:01 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,10 @@ require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   networks: {
-    altcoinchain: {
-      url: "https://rpc0.altcoinchain.org/rpc",
-      accounts: process.env.privateKey,
+    localhost: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "2330" // Match any network id
     }	    
 },
   solidity: "0.8.4",
